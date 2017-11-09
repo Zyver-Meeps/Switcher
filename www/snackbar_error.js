dfb92866e049a66800f18e7d53f1fe32eeee7f37
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2017-11-10 12:30:13 +1300

    1

--- www/snackbar_error.js
+++ www/snackbar_error.js
@@ -1,1 +1,11 @@
-﻿
+﻿function errorBar {
+
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar")
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
